908ea43e4ca738b40e1aa6b1731f0f8c63f4e3c7
Vishnu Velayuthan <100023569@mvla.net>
2021-04-22 10:09:53 -0700

    Getting some Navbar going

---
+++ src/HomePage/NavigationBar/NavigationBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import Navbar from "react-bootstrap/Navbar";
+
+export default class NaviagationBar extends Component { 
+    
+    render() {
+        return ;
+    }
+}
